f70109061a4e2b7101c6aa9659793ecb9b4d101b
Angel Angeles <daniel_1234@hotmail.es>
2023-10-22 16:32:22 -0400

    creating spinner tests

--- frontend/components/Spinner.test.js
+++ frontend/components/Spinner.test.js
@@ -1,5 +1,20 @@
 // Import the Spinner component into this file and test
 // that it renders what it should for the different props it can take.
-test('sanity', () => {
-  expect(true).toBe(false)
+import { render, screen } from '@testing-library/react'
+import React from "react";
+import Spinner from "./Spinner";
+
+
+describe(`Spinner tests`, () => {
+  const spinnerText = () => screen.queryByText('Please wait...')
+  
+  test('StyledSpinner is present when Spinner on is equal true.', () => {
+    render(<Spinner on={true}/>)
+    expect(spinnerText()).not.toEqual(null)
+  })
+  
+  test('StyledSpinner is not present when Spinner on is equal false.', () => {
+    render(<Spinner on={false}/>)
+    expect(spinnerText()).toEqual(null)
+  })
 })
